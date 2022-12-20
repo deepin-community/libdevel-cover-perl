29e4caf81515b21ee9a4609aca790933414e473a
Deepin Developer <deepin-dev@deepin.org>
2022-12-20 13:54:55 +0800

    feat: Init commit
    init libdevel-cover-perl
    
    Log:

---
+++ tests/Module_import.pm
@@ -0,0 +1,14 @@
+# Copyright 2002-2019, Paul Johnson (paul@pjcj.net)
+
+# This software is free.  It is licensed under the same terms as Perl itself.
+
+# The latest version of this software should be available from my homepage:
+# http://www.pjcj.net
+
+package Module_import;
+
+sub import {
+    print "Module_import\n";
+}
+
+1
